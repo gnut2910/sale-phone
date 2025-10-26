8222f23676ba757536f85f796e11f39c73c27505
gnut2910 <tunle4572@gmail.com>
2025-10-26 18:05:59 +0700

    test

--- frontend/src/apis/product.ts
+++ frontend/src/apis/product.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_URL, // backend API
+  baseURL: process.env.VITE_API_URL, // backend API
   headers: {
     "Content-Type": "application/json",
   },
