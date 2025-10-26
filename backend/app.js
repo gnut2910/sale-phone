a2db9b0cbdbd0d307046a5a3fbfb79390f932566
gnut2910 <tunle4572@gmail.com>
2025-10-26 17:45:41 +0700

    update cors + api url

--- backend/app.js
+++ backend/app.js
@@ -6,18 +6,17 @@ import brandRouter from "./src/routes/brand.route.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://myshop-nine-flax.vercel.app"], // domain frontend của bạn
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/uploads", express.static("src/uploads"));
 app.use("/api/products", productRouter);
 app.use("/api/brands", brandRouter);
-
-app.get("/test-db", (req, res) => {
-  connection.query("SELECT 1 + 1 AS result", (err, rows) => {
-    if (err) return res.status(500).json({ message: "DB Error", err });
-    res.json({ message: "Connected!", result: rows });
-  });
-});
 
 export default app;
